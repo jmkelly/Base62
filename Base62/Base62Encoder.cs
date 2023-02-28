f15d77768b4fb5199b364bb188fd092780db81f7
James Kelly <james.kelly.99@gmail.com>
2023-02-28 21:15:27 +1100

    reduce allocations

--- Base62/Base62Encoder.cs
+++ Base62/Base62Encoder.cs
@@ -6,17 +6,18 @@ namespace Base62
     public class Base62Encoder : IEncoder
     {
         readonly char[] charSet = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
+        private readonly StringBuilder sb;
         private const int bits = 62;
 
         public Base62Encoder()
         {
+            sb = new();
         }
 
         public string Encode(byte[] bytes)
         {
+            _ = sb.Clear();
             BigInteger number = new(bytes, isUnsigned: true, isBigEndian: true);
-            StringBuilder sb = new();
 
             while (number > 0)
             {
@@ -26,7 +27,7 @@ namespace Base62
                 number /= bits;
             }
 
-            var base62 = sb.ToString().ToCharArray();
+            char[] base62 = sb.ToString().ToCharArray();
             Array.Reverse(base62);
             return new string(base62);
         }
@@ -34,7 +35,7 @@ namespace Base62
         public string Encode(string value)
         {
             //get the binary in utf8
-            var bytes = Encoding.UTF8.GetBytes(value);
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
             return Encode(bytes);
         }
     }
